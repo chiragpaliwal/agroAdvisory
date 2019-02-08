0f4ddd2f70fb9914e7c9625ef0dbaad8da9df8e0
chiragpaliwal <chirag.paliwal06@gmail.com>
2019-02-08 14:35:24 +0530

    styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
-$("#tosignup").click(function (){
+$("#tosignup").click(function () {
     $("#loginform").slideToggle("slow");
     $("#signupform").delay(400).fadeToggle("slow");
 });
-$("#tologin").click(function(){
-$("#signupform").fadeToggle("fast");
-$("#loginform").delay(200).slideToggle("slow");
+$("#tologin").click(function () {
+    $("#signupform").fadeToggle("fast");
+    $("#loginform").delay(200).slideToggle("slow");
+});
+
+$(".tabs>div").click(function () {
+    $(".tabs>div:not(this)").removeClass("tabactive");
+    $(this).addClass("tabactive");
+});
+
+$("#tocrops").click(function () {
+    $(".profile").fadeToggle("fast");
+    $(".crops").delay(200).fadeToggle();
+});
+
+$("#toprofile").click(function () {
+    $(".crops").fadeToggle("fast");
+    $(".profile").delay(200).fadeToggle();
 });
